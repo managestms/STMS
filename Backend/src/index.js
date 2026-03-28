5eed868acde912666750658f09323f3e3cc2bef3
Taufique <taufique.231451.it@mhssce.ac.in>
2026-03-28 15:42:52 +0530

    t

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,27 +1,23 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
-import express from "express"
-import { app } from './app.js'
+import express from "express";
+import { app } from './app.js';
 import createDefaultUser from "./utils/defaultUser.js";
-
-
-//require('dotenv').config({path:'./env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-dotenv.config({
-    path: './env'
-})
+dotenv.config();
 
-connectDB()  //ye async use kiye hai to promise bhi return karega isliye
+connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 8080, () => {
-            console.log(`Server is running at port :${process.env.PORT}`);
+        const PORT = process.env.PORT || 8080;
 
-        })
+        app.listen(PORT, () => {
+            console.log(`Server is running at port :${PORT}`);
+        });
+
         createDefaultUser();
     })
     .catch((err) => {
         console.log("MONGODB connection failed !!!", err);
-
-    })
+        process.exit(1);
+    });
